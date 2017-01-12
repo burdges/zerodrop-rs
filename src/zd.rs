3ae065a6758d19aa392fed6b795d55cf46377ea1
Jeffrey Burdges <burdges@gnunet.org>
2017-01-12 11:23:39 -0500

    Remove Default bound for ZeroDrop since Rust lacks type level integers

---
+++ src/zd.rs
@@ -0,0 +1,193 @@
+// Copyright 2016 Jeffrey Burdges
+
+// //! Zeroing drop wrapper types.
+
+use std::boxed::Box;
+use std::ops::{Deref,DerefMut};
+use std::convert::{AsRef,AsMut};
+use std::borrow::{Borrow,BorrowMut};
+
+/// Zeroing drop wrapper type for `Copy` type.
+///
+/// Assuming `T: Copy`, a `ZeroDrop<T>` wraps a `Box<T>`
+/// and zeros it when dropped.  We must use `Box` because 
+/// LLVM moves data on the stack willy nilly.
+///
+/// ```rust
+/// let p : *const [u8; 32];
+/// let s = zerodrop::ZeroDrop::new_clone(&[3u8; 32]);  
+/// p = &*s;
+/// std::mem::drop(s);
+/// unsafe { assert_eq!(*p,[0u8; 32]); }
+/// ```
+///
+/// We recommend abstracting usage of `ZeroDrop` as follows because
+/// `ZeroDrop` does not `mlock` data.
+/// ```rust,ignore
+/// type Secret<T> = ZeroDrop<T> where T: Copy+Default;
+/// ```
+/// We similarly encurage wrapping `ZeroDrop` yourself so as to limit
+/// where and how secret data can be used in your code, including avoiding
+/// any trait magic that seems overly subtle.
+/// ```rust,ignore
+/// struct MySecret(pub ZeroDrop<[u8; 32]>);
+/// ```
+#[derive(Debug)]
+pub struct ZeroDrop<T>(Box<T>) where T: Copy;
+
+/// Zero a `ZeroDrop<T>` when dropped.
+impl<T> Drop for ZeroDrop<T> where T: Copy {
+    #[inline(never)]
+    fn drop(&mut self) {
+        let s: &mut T = self.0.deref_mut();
+        unsafe { ::std::intrinsics::volatile_set_memory::<T>(s,0,1) }
+        // We could do this if we had default
+        // *self.0 = Default::default();
+    }
+}
+
+/// Create a `ZeroDrop<T>` for a `T: Copy` consisting of a `Box<T>`
+/// that will be zeroed when dropped. 
+impl<T> ZeroDrop<T> where T: Copy {
+    pub fn new_default() -> ZeroDrop<T> where T: Default {
+        ZeroDrop(Default::default())
+    }
+
+    /// Insecure if `t` likely gets placed on the stack
+    pub fn new_insecure(t: T) -> ZeroDrop<T> {
+        ZeroDrop(Box::new(t))
+    }
+
+    pub unsafe fn new_uninitialized() -> ZeroDrop<T> {
+        ZeroDrop(Box::new(::std::mem::uninitialized::<T>()))
+    }
+
+    pub fn new_clone(t: &T) -> ZeroDrop<T> {
+        let mut b = Box::new(unsafe { ::std::mem::uninitialized::<T>() });
+        unsafe { ::std::ptr::copy_nonoverlapping::<T>(t,b.deref_mut(),1) }
+        ZeroDrop(b)
+    }
+
+    pub unsafe fn zero_out(&mut self) {
+        let s: &mut T = self.0.deref_mut();
+        ::std::intrinsics::volatile_set_memory::<T>(s,0,1)
+    }
+
+    pub fn new_zeroed() -> ZeroDrop<T> {
+        // let mut z = unsafe { Self::new_uninitialized() };
+        // unsafe { z.zero_out() }
+        let mut b = Box::new(unsafe { ::std::mem::uninitialized::<T>() });
+        unsafe { ::std::intrinsics::volatile_set_memory::<T>(b.deref_mut(),0,1) }
+        ZeroDrop(b)
+    }
+}
+
+impl<T> Default for ZeroDrop<T> where T: Copy+Default {
+    fn default() -> ZeroDrop<T> {
+        ZeroDrop(Default::default())
+    }
+}
+
+/// `Clone` the underlying `Box`
+impl<T> Clone for ZeroDrop<T> where T: Copy {
+    fn clone(&self) -> ZeroDrop<T> {
+        ZeroDrop(self.0.clone())
+    }
+    fn clone_from(&mut self, source: &ZeroDrop<T>) {
+        self.0.clone_from(&source.0);
+    }
+}
+
+/// Delegate `Deref` to `Box`
+impl<T> Deref for ZeroDrop<T> where T: Copy {
+    type Target = T;
+
+    fn deref(&self) -> &T {
+        self.0.deref()
+    }
+}
+
+/// Delegate `DerefMut` to `Box`
+impl<T> DerefMut for ZeroDrop<T> where T: Copy {
+    fn deref_mut(&mut self) -> &mut T {
+        self.0.deref_mut()
+    }
+}
+
+/// Delegate `AsRef<_>` to `Box`
+impl<T,U> AsRef<U> for ZeroDrop<T> where T: Copy+AsRef<U> {
+    fn as_ref(&self) -> &U {
+        self.0.as_ref().as_ref()
+    }
+}
+
+/// Delegate `AsMut<_>` to `Box`
+impl<T,U> AsMut<U> for ZeroDrop<T> where T: Copy+AsMut<U> {
+    fn as_mut(&mut self) -> &mut U {
+        self.0.as_mut().as_mut()
+    }
+}
+
+/// Delegate `Borrow<_>` to `Box`
+impl<T> Borrow<T> for ZeroDrop<T> where T: Copy {
+    fn borrow(&self) -> &T {
+        self.0.borrow()
+    }
+}
+
+/// Delegate `BorrowMut<_>` to `Box`
+impl<T> BorrowMut<T> for ZeroDrop<T> where T: Copy {
+    fn borrow_mut(&mut self) -> &mut T {
+        self.0.borrow_mut()
+    }
+}
+
+
+
+/*
+trait ConstantTimeEq {
+    fn constant_time_eq(a: &Self, b: &Self) -> bool;
+}
+
+impl<T> ConstantTimeEq for [T] where T: ConstantTimeEq {
+    fn constant_time_eq(x: &Self, y: &Self) -> bool {
+        ;
+    }
+}
+
+/// We implement `PartialEq` to be a constant time comparison, so that
+/// noone may define it otherwise.  
+impl<T> PartialEq for ZeroDrop<T> where T: ConstantTimeEq {
+    fn eq(&self, other: &ZeroDrop<T>) -> bool {
+        ::consistenttime::ct_u8_slice_eq(&self.0, &other.0)
+    }
+}
+impl<T> Eq for Secret<T>  where T: Copy { }
+*/
+
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn zeroing_drops() {
+        let p : *const [u8; 32];
+        let s = ZeroDrop::new_insecure([3u8; 32]);  
+        p = s.deref();
+        ::std::mem::drop(s);
+        unsafe { assert_eq!(*p,[0u8; 32]); }
+    }
+    #[test]
+    #[should_panic(expected = "assertion failed")]
+    fn not_droped() {
+        let p : *const [u8; 32];
+        let s = ZeroDrop::new_insecure([3u8; 32]);  
+        p = s.deref();
+        // ::std::mem::drop(s);
+        unsafe { assert_eq!(*p,[0u8; 32]); }
+    }
+}
+
+
---
+++ src/zdd.rs
@@ -0,0 +1,167 @@
+// Copyright 2016 Jeffrey Burdges
+
+// //! Zeroing drop wrapper types.
+
+use std::boxed::Box;
+use std::ops::{Deref,DerefMut};
+use std::convert::{AsRef,AsMut};
+use std::borrow::{Borrow,BorrowMut};
+
+/// Zeroing drop wrapper type for `Drop` types.
+///
+/// `ZeroDropDrop<T>` wraps a `Box<T>` where `T: Drop+Default`.
+/// It's drop method invokes `<T as Drop>::drop` before replacing
+/// `T` with `<T as Default>::default()`, which gets dropped again.
+///
+/// We warn `ZeroDropDrop<T>` cannot deeply zero data, so do not
+/// use it with objects like `Vec<T>`, `HashMap<T>`, etc.
+/// For those, you'll need a custom allocator or a whole custom
+/// data structure.
+#[derive(Debug)]
+pub struct ZeroDropDrop<T>(Box<T>) where T: Drop+Default;
+
+/// Zero a `ZeroDrop<T>` when dropped.
+impl<T> Drop for ZeroDropDrop<T> where T: Drop+Default {
+    #[inline(never)]
+    fn drop(&mut self) {
+        let s: &mut T = self.0.deref_mut();
+        unsafe {
+            ::std::ptr::drop_in_place::<T>(s);
+            ::std::ptr::write::<T>(s,Default::default());
+        }
+    }
+}
+
+/// Create a `ZeroDropDrop<T>` for a `T: Drop` that invokes 
+/// `<T as Drop>::drop` before zeroing `T`.  We observe that
+/// `<T as Drop>::drop` will be invoked a second time on a
+/// `<T as Default>::default()` for each drop, so this must
+/// be safe and desirable.  `ZeroDropDrop<T>` does not 
+/// deeply zero data, so do not use it with data type that
+/// employ allocation, like `Vec<T>`, `HashMap<T>`, etc.
+impl<T> ZeroDropDrop<T> where T: Drop+Default {
+    pub fn new_default() -> ZeroDropDrop<T> {
+        ZeroDropDrop(Default::default())
+    }
+
+    pub unsafe fn new_uninitialized() -> ZeroDropDrop<T> {
+        ZeroDropDrop(Box::new(::std::mem::uninitialized::<T>()))
+    }
+
+    pub unsafe fn zero_out(&mut self) {
+        let s: &mut T = self.0.deref_mut();
+        ::std::intrinsics::volatile_set_memory::<T>(s,0,1)
+    }
+
+    // Is b.clone_from(t) safe when b is uninitialized?
+    /*
+    pub fn new_clone(t: &T) -> ZeroDrop<T> {
+        let mut b = Box::new(unsafe { ::std::mem::uninitialized::<T>() });
+        b.clone_from(t);
+        ZeroDrop(b)
+    }
+    */
+}
+
+/// `Clone` the underlying `Box`
+impl<T> Clone for ZeroDropDrop<T> where T: Drop+Default+Clone {
+    fn clone(&self) -> ZeroDropDrop<T> {
+        ZeroDropDrop(self.0.clone())
+    }
+    fn clone_from(&mut self, source: &ZeroDropDrop<T>) {
+        self.0.clone_from(&source.0);
+    }
+}
+
+/// Delegate `Deref` to `Box`
+impl<T> Deref for ZeroDropDrop<T> where T: Drop+Default {
+    type Target = T;
+
+    fn deref(&self) -> &T {
+        self.0.deref()
+    }
+}
+
+/// Delegate `DerefMut` to `Box`
+impl<T> DerefMut for ZeroDropDrop<T> where T: Drop+Default {
+    fn deref_mut(&mut self) -> &mut T {
+        self.0.deref_mut()
+    }
+}
+
+/// Delegate `AsRef<_>` to `Box`
+impl<T,U> AsRef<U> for ZeroDropDrop<T> where T: Drop+Default+AsRef<U> {
+    fn as_ref(&self) -> &U {
+        self.0.as_ref().as_ref()
+    }
+}
+
+/// Delegate `AsMut<_>` to `Box`
+impl<T,U> AsMut<U> for ZeroDropDrop<T> where T: Drop+Default+AsMut<U> {
+    fn as_mut(&mut self) -> &mut U {
+        self.0.as_mut().as_mut()
+    }
+}
+
+/// Delegate `Borrow<_>` to `Box`
+impl<T> Borrow<T> for ZeroDropDrop<T> where T: Drop+Default {
+    fn borrow(&self) -> &T {
+        self.0.borrow()
+    }
+}
+
+/// Delegate `BorrowMut<_>` to `Box`
+impl<T> BorrowMut<T> for ZeroDropDrop<T> where T: Drop+Default {
+    fn borrow_mut(&mut self) -> &mut T {
+        self.0.borrow_mut()
+    }
+}
+
+
+/*
+trait ConstantTimeEq {
+    fn constant_time_eq(a: &Self, b: &Self) -> bool;
+}
+
+impl<T> ConstantTimeEq for [T] where T: ConstantTimeEq {
+    fn constant_time_eq(x: &Self, y: &Self) -> bool {
+        ;
+    }
+}
+
+/// We implement `PartialEq` to be a constant time comparison, so that
+/// noone may define it otherwise.  
+impl<T> PartialEq for ZeroDrop<T> where T: ConstantTimeEq {
+    fn eq(&self, other: &ZeroDrop<T>) -> bool {
+        ::consistenttime::ct_u8_slice_eq(&self.0, &other.0)
+    }
+}
+impl<T> Eq for Secret<T>  where T: Copy { }
+*/
+
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn zeroing_drops() {
+        let p : *const [u8; 32];
+        let s = ZeroDrop::new_insecure([3u8; 32]);  
+        p = s.deref();
+        ::std::mem::drop(s);
+        unsafe { assert_eq!(*p,[0u8; 32]); }
+    }
+    #[test]
+    #[should_panic(expected = "assertion failed")]
+    fn not_droped() {
+        let p : *const [u8; 32];
+        let s = ZeroDrop::new_insecure([3u8; 32]);  
+        p = s.deref();
+        // ::std::mem::drop(s);
+        unsafe { assert_eq!(*p,[0u8; 32]); }
+    }
+}
+
+
